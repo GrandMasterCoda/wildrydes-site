94d01449441d7b2c56f3a0ccb9ce895cb814635f
Ori Hytner <165577983+GrandMasterCoda@users.noreply.github.com>
2025-06-27 12:49:44 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_8a3OZ07zP', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-central-1_0sE1jpRCk', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1m0kgrncmormi6tq32poredr5t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
